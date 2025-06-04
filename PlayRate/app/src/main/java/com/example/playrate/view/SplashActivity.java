9100798c9af423148a70af77d947658a54e7755a
DiegoBlancoMos <dblancom23@fpcoruna.afundacion.org>
2025-06-04 18:10:36 +0200

    SplashActivity creada.

---
+++ PlayRate/app/src/main/java/com/example/playrate/view/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.playrate.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.playrate.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int DURACION_SPLASH = 2000; // 2 segundos
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, DURACION_SPLASH);
+    }
+}
